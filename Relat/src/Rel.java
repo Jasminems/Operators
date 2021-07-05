d9b491606bd43a30806cffda7f3d28bab49ce194
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 19:11:18 +0530

    Relarional

---
+++ Relat/src/Rel.java
@@ -0,0 +1,13 @@
+/*A Java program to perform Relational Operator*/
+class Rel 
+{
+	public static void main(String[] args) 
+	{
+		int a=10,b=20;
+		System.out.println("a>b:"+(a>b));
+		System.out.println("a<b:"+(a<b));
+		System.out.println("a<=b:"+(a<=b));
+		System.out.println("a>=b:"+(a>=b));
+	}
+
+}
