c717f3a534c265f48cd9f3936d1ab607f9815343
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 18:25:34 +0530

    Operators

---
+++ IDOper/src/Idop.java
@@ -0,0 +1,13 @@
+/*A Java program to perform increment and decrement operator*/
+class Idop 
+{
+	public static void main(String[] args) 
+	{
+		int n=10;
+		System.out.println("Number is:"+n);
+		n++;
+		System.out.println("Now the number is:"+n);
+		n--;
+		System.out.println("Now the number is:"+n);
+	}
+}
