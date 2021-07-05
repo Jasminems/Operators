6a7296de553d7dda785533cd86eefe3fb44d6929
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 19:06:43 +0530

    Logical Not

---
+++ Lognot/src/Lonot.java
@@ -0,0 +1,13 @@
+/*A Java program to perform Logical NOT*/
+class Lonot 
+{
+	public static void main(String[] args) 
+	{
+		int a=10,b=20;
+		System.out.println("A:"+a);
+		System.out.println("B:"+b);
+		System.out.println("!(a<b)="+!(a<b));
+		System.out.println("!(a>b)="+!(a>b));
+	}
+
+}
