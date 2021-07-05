d557fc1184384df6989216772e19380e45c8efc7
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 19:04:49 +0530

    Logical OR

---
+++ Logior/src/loor.java
@@ -0,0 +1,18 @@
+/*A Java program to perform Logical OR*/
+class loor 
+{
+	public static void main(String[] args) 
+	{
+		int a=10,b=1,c=10,d=30;
+		System.out.println("A:"+a);
+		System.out.println("B:"+b);
+		System.out.println("C:"+c);
+		System.out.println("D:"+d);
+		if(a>b||c==d)
+			System.out.println("One or both the conditions are true");
+		else
+			System.out.println("Both the conditions are false");
+
+	}
+
+}
