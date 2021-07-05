a54f81d9d0cb485e5e4094778a0df59780c41d7c
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 19:00:03 +0530

    Equal or Not equal

---
+++ Eqnot/src/Enop.java
@@ -0,0 +1,19 @@
+/*A Java program to find 2 numbers equal or not*/
+class Enop 
+{
+	public static void main(String[] args) 
+	{
+		int n1=10;
+		int n2=20;
+		System.out.println("The First number is:"+n1);
+		System.out.println("The Second numberr is:"+n2);
+		if(n1==n2)//using equal operator
+		{
+			System.out.println("The numbers are equal");
+		}
+		else if(n1!=n2)//using not equal operator
+		{
+			System.out.println("The numbers are not equal");
+		}
+	}
+}
